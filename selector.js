d9392b61ab0dc6598279c4467e14f49de1e1b015
Ulises Vargas <ext_uvargasds@falabella.cl>
2023-08-02 17:42:00 -0400

    fix/changes-datasource

--- selector.js
+++ selector.js
@@ -61,7 +61,7 @@ export default class CardExample extends Component {
   }
 
   onChange(itemValue) {
-    console.log("itemValue", itemValue);
+    console.log("itemValue", itemValue.value);
     this.setState({
       selectedValue: itemValue,
       coin_name: itemValue.name,
